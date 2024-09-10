af883553cf94704611b8cb70b90554c8d9274fb2
Vedant363 <vedant.ghumade363@gmail.com>
2024-09-11 02:51:35 +0530

    Home page

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons';
+
+const InfoBox = ({ text, link, btnText }) => (
+    <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>{text}</p>
+      <Link to={link} className='neo-brutalism-white neo-btn'>{btnText}
+      <img src={arrow} className='w-4 h-4 object-contain'/>
+      </Link>
+    </div>
+)
+
+const renderContent = {
+    1: (
+        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+            Hi, I am <span className='font-semibold'>Vedant</span> 👋🏼
+            <br />
+            <br />
+            A Full Stack Developer
+        </h1>
+    ),
+    2: (
+        <InfoBox 
+          text="Currently pursuing B.Tech in Computer Science and Engineering from PCCOE."
+          link='/about'
+          btnText='Learn More'
+        />
+    ),
+    3: (    
+        <InfoBox 
+          text="Learn about my projects and contributions"
+          link='/projects'
+          btnText='See Projects'
+        />
+    ),
+    4: (
+        <InfoBox 
+          text="Want to contact me? Let's connect!"
+          link='/contact'
+          btnText='Contact Me'
+        />
+    )
+}
+
+
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo
