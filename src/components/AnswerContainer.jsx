75b1f411e5ef42579b457624405bf69353abcb90
Vedant363 <vedant.ghumade363@gmail.com>
2025-01-06 09:35:21 +0530

    small bug fix

--- src/components/AnswerContainer.jsx
+++ src/components/AnswerContainer.jsx
@@ -4,7 +4,7 @@ import Markdown from 'react-markdown';
 const AnswerContainer = ({ answer, theme }) => {
   return (
     <div id='root' className="answercontainer p-4 mt-[4.688rem] h-auto w-full max-w-full overflow-x-auto border-2 border-blue-600 shadow-lg shadow-blue-500 rounded-lg">
-      <div className={`markdown-container ${theme}-markdown-container w-full max-w-full overflow-x-auto ${theme === "light" ? "text-black" : "text-white"}`}>
+      <div className={`markdown-container ${theme}-markdown-container ${theme === "light" ? "text-black" : "text-white"}`}>
         <Markdown>{answer}</Markdown>
       </div>
     </div>
